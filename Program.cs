33211b9b21359c9c68040f06a2df1ed7371b07e0
noxtopher <christophernock@me.com>
2015-11-18 20:24:50 +0000

    Revert "broke"
    This reverts commit 4393f3ce7812cfd300c59aa155faf63229088270.

--- Program.cs
+++ Program.cs
@@ -1,50 +1,24 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Text.RegularExpressions;
 
-namespace Poo
+namespace AirSonos
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Chris' stupid App to ping stuff");
+			Console.WriteLine ("StartingAirSonos");
 
 			{
 				Process[] processes = Process.GetProcessesByName("cmd");
 				foreach (var process in processes) {
-				//process.Kill();
+					process.Kill();
 
 				}
-
-				Console.WriteLine ("Enter an IP to Ping then press Enter");
-				string hostname = " " + Console.ReadLine();
-				Console.WriteLine ("Please Wait...");
-				//string hostname = "google.com".PadLeft(11);
-				ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + "ping" + hostname);
-				Process proc = new Process();
-				procStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-				procStartInfo.UseShellExecute = false;
-				procStartInfo.RedirectStandardOutput = true;
-				//procStartInfo.CreateNoWindow = true;
-				proc.StartInfo = procStartInfo;
-				proc.Start();
-				string output2 = proc.StandardOutput.ReadToEnd();
-				proc.WaitForExit();
-				//Console.WriteLine (output2);
-
-				// ... One or more digits.
-				Match m = Regex.Match(output2, @"Reply");
-
-				// ... Write value.
-				if (m.Value =="Reply") Console.WriteLine("Ping was sucessful");
-				else
-					Console.WriteLine("Ping was not sucessful");
-				//var process2 = Process.Start ("cmd.exe", @"/c ping 8.8.8.8");
-				//process2.WaitForExit();
-				//System.Threading.Thread.Sleep (5000);
-				Console.WriteLine("Press enter to close...");
-				Console.ReadLine();
+				
+				var process2 = Process.Start ("cmd.exe", @"/c C:\Users\Screen\AppData\Roaming\npm\airsonos.cmd");
+				process2.WaitForExit();
+				System.Threading.Thread.Sleep (5000);
 
 			}
 
@@ -54,5 +28,6 @@ namespace Poo
 		}
 	}
 }
-	
+
+
 
