4393f3ce7812cfd300c59aa155faf63229088270
noxtopher <christophernock@me.com>
2015-11-18 20:24:41 +0000

    broke

--- Program.cs
+++ Program.cs
@@ -1,24 +1,50 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
-namespace AirSonos
+namespace Poo
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("StartingAirSonos");
+			Console.WriteLine ("Chris' stupid App to ping stuff");
 
 			{
 				Process[] processes = Process.GetProcessesByName("cmd");
 				foreach (var process in processes) {
-					process.Kill();
+				//process.Kill();
 
 				}
-				
-				var process2 = Process.Start ("cmd.exe", @"/c C:\Users\Screen\AppData\Roaming\npm\airsonos.cmd");
-				process2.WaitForExit();
-				System.Threading.Thread.Sleep (5000);
+
+				Console.WriteLine ("Enter an IP to Ping then press Enter");
+				string hostname = " " + Console.ReadLine();
+				Console.WriteLine ("Please Wait...");
+				//string hostname = "google.com".PadLeft(11);
+				ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + "ping" + hostname);
+				Process proc = new Process();
+				procStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+				procStartInfo.UseShellExecute = false;
+				procStartInfo.RedirectStandardOutput = true;
+				//procStartInfo.CreateNoWindow = true;
+				proc.StartInfo = procStartInfo;
+				proc.Start();
+				string output2 = proc.StandardOutput.ReadToEnd();
+				proc.WaitForExit();
+				//Console.WriteLine (output2);
+
+				// ... One or more digits.
+				Match m = Regex.Match(output2, @"Reply");
+
+				// ... Write value.
+				if (m.Value =="Reply") Console.WriteLine("Ping was sucessful");
+				else
+					Console.WriteLine("Ping was not sucessful");
+				//var process2 = Process.Start ("cmd.exe", @"/c ping 8.8.8.8");
+				//process2.WaitForExit();
+				//System.Threading.Thread.Sleep (5000);
+				Console.WriteLine("Press enter to close...");
+				Console.ReadLine();
 
 			}
 
@@ -28,6 +54,5 @@ namespace AirSonos
 		}
 	}
 }
-
-
+	
 
